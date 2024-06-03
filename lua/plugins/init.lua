312873075a64899b6c8b511b519a53aba9ef98d4
Rakesh Sanghvi <rakesh.sanghvi@curantissolutions.com>
2024-06-03 09:44:47 -0500

    added command line auto completion

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -123,6 +123,31 @@ return {
   'ThePrimeagen/vim-be-good',
   lazy = false
 },
+
+{
+    "hrsh7th/nvim-cmp",
+    event = { "InsertEnter", "CmdLineEnter" },
+    dependencies = {
+      'hrsh7th/cmp-cmdline',
+    },
+    opts = function(_, opts)
+      local cmp = require('cmp')
+      -- `:` cmdline setup.
+      cmp.setup.cmdline(':', {    
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = cmp.config.sources({
+          { name = 'path' }
+        }, {
+          {
+            name = 'cmdline',
+            option = {
+              ignore_cmds = { 'Man', '!' }
+            }
+          }
+        })
+      })
+    end,
+  },
 -- {
 --     "ThePrimeagen/harpoon",
 --     branch = "harpoon2",
